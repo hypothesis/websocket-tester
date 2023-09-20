fa03f0c30392601468bba18be1f0622b734bba21
Robert Knight <robertknight@gmail.com>
2023-09-20 11:47:47 +0100

    Add a ping/pong test
    The ping/pong test exercises the connection via built-in protocol features
    without involving the work queue etc. inside the H WebSocket server.

--- websocket_tester/__main__.py
+++ websocket_tester/__main__.py
@@ -13,18 +13,31 @@ def run_test(endpoint="wss://h-websocket.hypothes.is/ws"):
     n_successes = 0
     n_timeouts = 0
     next_msg_id = 0
+    timeout = 3
 
     for n_attempt in range(max_attempts):
         next_msg_id += 1
         print(f"Attempt {n_attempt} of {max_attempts}..")
         with connect(endpoint) as websocket:
+            # Simple ping-pong test, to verify the connection was established.
+            try:
+                pong_event = websocket.ping()
+                pong_event.wait(timeout=timeout)
+            except TimeoutError:
+                print("Ping timed out")
+                n_timeouts += 1
+                continue
+
+            # Send a "whoami" message. On the H websocket this involves the
             websocket.send(json.dumps({"type": "whoami", "id": next_msg_id}))
             try:
-                message = websocket.recv(timeout=3)
-                print(f"Received response {message}")
+                recv_start = time.time()
+                message = websocket.recv(timeout=timeout)
+                recv_elapsed = time.time() - recv_start
+                print(f"Received response {message} in {recv_elapsed:.2f}s")
                 n_successes += 1
             except TimeoutError:
-                print("Timed out")
+                print('"whoami" request timed out')
                 n_timeouts += 1
 
     print(
